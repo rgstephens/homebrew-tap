0a7a453a7833cbf228e7fac30abb20b4afe62bdc
Greg Stephens <greg@udon.org>
2026-04-14 22:39:55 +0000

    Update ghin formula to 0.3.0

--- Formula/ghin.rb
+++ Formula/ghin.rb
@@ -4,21 +4,21 @@
 class Ghin < Formula
   desc "CLI for the GHIN golf handicap API"
   homepage "https://github.com/rgstephens/ghin"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.2.0/ghin_0.2.0_darwin_amd64.tar.gz"
-      sha256 "3c0eef6c65fe0f1b7b46a4a445b882dc2b369c169d115e90ad366df808ba094b"
+      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.3.0/ghin_0.3.0_darwin_amd64.tar.gz"
+      sha256 "c85c855fc2b4666a56d725bfac0caea5af15a7f64a491a41f01d3d4e02076ed7"
 
       def install
         bin.install "ghin"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.2.0/ghin_0.2.0_darwin_arm64.tar.gz"
-      sha256 "c8faa6bed0263e66e088fe099d8ed738827af94a758b3231a31a02ebdc8a49be"
+      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.3.0/ghin_0.3.0_darwin_arm64.tar.gz"
+      sha256 "a96334c980d9916fb77cf3211150a7f3082ffdfac1d4430f686060073b7dba0f"
 
       def install
         bin.install "ghin"
@@ -28,16 +28,16 @@ class Ghin < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.2.0/ghin_0.2.0_linux_amd64.tar.gz"
-      sha256 "478e4c483de5554de9a6c9fe8d44d0ed066241e50a5392470f7c65b14c24a7fd"
+      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.3.0/ghin_0.3.0_linux_amd64.tar.gz"
+      sha256 "749824ce75329ab74bdb252408ee29cb8a60d8f299c5ff83585c268f84a157de"
 
       def install
         bin.install "ghin"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.2.0/ghin_0.2.0_linux_arm64.tar.gz"
-      sha256 "06ec9054ed135752923c737393822026a27b03dff6bfdbc44c9595012f6364ee"
+      url "https://github.com/rgstephens/homebrew-tap/releases/download/ghin-v0.3.0/ghin_0.3.0_linux_arm64.tar.gz"
+      sha256 "602f6a72404091cf263aba06a01dbad00ffc1f290e76b6521c632c07b771b05c"
 
       def install
         bin.install "ghin"
